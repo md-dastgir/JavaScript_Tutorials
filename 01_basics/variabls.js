030a334f79caf339e50fb4a30d3bea38076a8cbb
md-dastgir <mddastagir896@gmail.com>
2025-08-01 13:04:28 +0000

    01_variables

---
+++ 01_basics/variabls.js
@@ -0,0 +1,10 @@
+const accountId = 1234;
+let email = "abc123@gmail.com";
+var password = "232323";
+city = "Delhi";
+let test;
+
+// accountId = 1221;    we cannot change const value.
+console.log(accountId);
+
+console.table([accountId, email, password, city, test]);
