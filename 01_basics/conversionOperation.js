e70eaa5ef04fa26ab410549ed859b2c2bb41b11b
md-dastgir <mddastagir896@gmail.com>
2025-08-01 13:34:41 +0000

    ConversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let convert = Number(score);
+
+console.log(typeof convert);
+
+console.log(convert);
+
+
+/*
+
+"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+1 => true, 0 => false
+"" (Empty string) => false
+" " (Not Empty string) => true
+*/
