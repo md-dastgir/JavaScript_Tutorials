ea6817822582ffd44c745add1210df72ccd38e2f
md-dastgir <mddastagir896@gmail.com>
2025-08-01 13:49:59 +0000

    02_ConversionOperation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -19,3 +19,23 @@ true => 1, false => 0
 "" (Empty string) => false
 " " (Not Empty string) => true
 */
+
+
+
+/*
+
+prefix and postfix
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+*/
