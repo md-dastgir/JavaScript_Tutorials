08ee21bbcd155419e4d5db10297b4960b1b695fe
md-dastgir <mddastagir896@gmail.com>
2025-08-02 10:17:54 +0000

    Template Literals

---
+++ 02_JS/temLitrels.js
@@ -0,0 +1,9 @@
+//Template Literals
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+
+let output = `The total Price is: ${pencilPrice + erasorPrice} Rupee.`;
+console.log(output);
+
+//  ``(back tic)
