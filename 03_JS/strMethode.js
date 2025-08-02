f20ac5f4fd01142c1a0ecf9fa47036019ab09ad3
md-dastgir <mddastagir896@gmail.com>
2025-08-02 10:59:53 +0000

    trim Method in string

---
+++ 03_JS/strMethode.js
@@ -0,0 +1,10 @@
+/*
+
+String Methods
+method => actions that can performed on objects
+
+
+Formate
+
+stringName.method()
+*/
---
+++ 03_JS/trim.js
@@ -0,0 +1,8 @@
+let msg = "    Hello    ";
+
+console.log(msg);       // o/p => '     Hello     '
+console.log(msg.trim());        // o/p => 'Hello'
+
+let newMsg = "   hel  lo   ";
+
+console.log(newMsg.trim());     // o/p => 'hel  lo'
