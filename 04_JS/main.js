28b8e2f2963ab45aa55f9b86146888ee11fb777c
Md Dastgir <mddastagir896@gmail.com>
2025-08-04 04:59:16 +0000

    event of js

---
+++ 04_JS/main.js
@@ -0,0 +1,15 @@
+function color(colorValue){
+    
+    
+    var red = document.getElementById("red");
+    red.style.backgroundColor = colorValue;
+
+    var green = document.getElementById("green");
+    green.style.backgroundColor = colorValue;
+
+    var orange = document.getElementById("orange");
+    orange.style.backgroundColor = colorValue;
+
+    var blue = document.getElementById("blue");
+    blue.style.backgroundColor = colorValue;
+}
